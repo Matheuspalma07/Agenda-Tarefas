ad8feda938df85986ae15551a7ebee903550813c
Matheus Palma <matheus.palma07@gmail.com>
2022-08-18 20:56:47 -0300

    Inicio do projeto

---
+++ AgendaTarefas/Models/Contexto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaTarefas.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        IConvertToActionResult
+
+
+
+       
+
+
+        
+
+
+        
+
+
+       
+
+
+        
+
+        
+        
+            
+
+    }
+}
